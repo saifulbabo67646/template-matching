881954f34246f5f1c7818ff3700264782a75e8cc
Nafis Handoko <naffisioner@gmail.com>
2022-08-07 22:30:24 +0700

    convert FileStorage object to cv2 readable file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+import cv2, numpy as np
 app = Flask(__name__)
 CORS(app)
 
@@ -10,11 +11,16 @@ def tes():
 @app.route('/api/tm', methods=["POST"])
 def tm():
     # print('tesaja')
-    file = request.files['image']
-    if file:
-        print(file)
+    image_req = request.files['image'].read()
+    template_req = request.files['template'].read()
+    if image_req and template_req:
+        img_bytes = np.fromstring(image_req, np.uint8)
+        template_bytes = np.fromstring(template_req, np.uint8)
+        img = cv2.imdecode(img_bytes, cv2.IMREAD_UNCHANGED)
+        template = cv2.imdecode(template_bytes, cv2.IMREAD_UNCHANGED)
+        print(img)
         return "OK"
     return "not ok"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
