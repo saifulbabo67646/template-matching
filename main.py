a38100f68166b1073d97ade50957c6dfdb03804d
Nafis Handoko <naffisioner@gmail.com>
2022-08-07 20:37:47 +0700

    succesfully received the file

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def tes():
     return 'Halo dunia!'
+
+@app.route('/api/tm', methods=["POST"])
+def tm():
+    # print('tesaja')
+    file = request.files['image']
+    if file:
+        print(file)
+        return "OK"
+    return "not ok"
 
 if __name__ == '__main__':
     app.run()
